082b57ce5e86a01b751eff0b05b158251d8c2cb9
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-10-29 14:11:37 +0100

    Add project files.

---
+++ PASSCloudWebApi/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PASSCloudWebApi.Models;
+
+namespace PASSCloudWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AdventureWorksLTContext _context;
+
+        public CustomersController(AdventureWorksLTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers/email
+        [HttpGet("{email}")]
+        public ActionResult<CustomerShort> GetCustomer(string email)
+        {
+            var customer = _context.Customer.FirstOrDefault(x => x.EmailAddress == email);
+
+            if (customer != null)
+            {
+                return new CustomerShort()
+                {
+                    EmailAddress = customer.EmailAddress,
+                    CompanyName = customer.CompanyName,
+                    CustomerName = $"{customer.Title} {customer.FirstName} {customer.LastName} {customer.Suffix}"
+                };
+            }
+
+            return default(CustomerShort);
+        }
+    }
+}
---
+++ PASSCloudWebApi/Models/CustomerShort.cs
@@ -0,0 +1,9 @@
+﻿namespace PASSCloudWebApi.Models
+{
+    public class CustomerShort
+    {
+        public string CustomerName { get; set; }
+        public string CompanyName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
